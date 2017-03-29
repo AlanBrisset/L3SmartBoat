edca22142e86568539789fee84d4e5854a19c8fb
Arthur Brugiere <arbr1@hotmail.fr>
2017-03-29 08:57:57 +0200

    Create class Waypoint

---
+++ L3smartBoat/L3smartBoat/Waypoint.h
@@ -0,0 +1,13 @@
+//
+//  Waypoint.h
+//  L3smartBoat
+//
+//  Created by Arthur on 20/07/1733 ERA1.
+//  Copyright © 1733 ERA1 Jerome Godefroy. All rights reserved.
+//
+
+#ifndef Waypoint_h
+#define Waypoint_h
+
+
+#endif /* Waypoint_h */
---
+++ L3smartBoat/L3smartBoat/Waypoint.m
@@ -0,0 +1,9 @@
+//
+//  Waypoint.m
+//  L3smartBoat
+//
+//  Created by Arthur on 20/07/1733 ERA1.
+//  Copyright © 1733 ERA1 Jerome Godefroy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
